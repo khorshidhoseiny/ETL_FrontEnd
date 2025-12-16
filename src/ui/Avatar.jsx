be587615d2054be4df7ca9a8b9cce6a80daec43d
khorshid-h <www.khorshid11@gmail.com>
2025-12-16 15:49:17 +0330

    avatar component was changed

--- src/ui/Avatar.jsx
+++ src/ui/Avatar.jsx
@@ -11,7 +11,7 @@ function Avatar({ Img, name, Role }) {
       </div>
       <img
         src={Img}
-        className="w-12 h-12  rounded-full ring-1 ring-gray-300"
+        className="w-14 h-14 rounded-full ring ring-gray-300"
         alt="profile"
       />
     </div>
